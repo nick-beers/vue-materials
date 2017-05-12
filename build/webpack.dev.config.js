a1b6e6829aa588e401fac5f44634c6503d520982
Nick Beers <nicholas.beers@gmail.com>
2017-05-12 11:50:20 -0400

    Update modules and fixing dev config to work with newest webpack. setup.

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -9,17 +9,27 @@ module.exports = {
     filename: 'dev.js'
   },
   resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
+    modules: [__dirname, 'node_modules']
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.vue$/,
-        loader: 'vue'
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+          // other vue-loader options go here
+        }
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules|dist/
       },
       {
@@ -42,6 +52,9 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     noInfo: true
+  },
+  performance: {
+    hints: false
   },
   devtool: '#eval-source-map'
 }
