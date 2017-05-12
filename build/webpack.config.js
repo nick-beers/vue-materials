29c074121b2da57cc06f69ea9a6b6b7224aba926
Nick Beers <nicholas.beers@gmail.com>
2017-05-12 12:32:04 -0400

    Updating official build to work with webpack 2.x ; map file was not updated

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -10,17 +10,26 @@ module.exports = {
     libraryTarget: 'umd'
   },
   resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
+    modules: [__dirname, 'node_modules']
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.vue$/,
-        loader: 'vue'
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+        }
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
@@ -43,6 +52,9 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     noInfo: true
+  },
+  performance: {
+    hints: false
   },
   devtool: '#eval-source-map'
 }
