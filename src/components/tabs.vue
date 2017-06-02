698971ca8b2f2c4d44e3c1a803dd1486f5db5b6c
Nick Beers <nicholas.beers@gmail.com>
2017-06-02 12:30:31 -0400

    Updating tabs options #2 and allowing programatic control of selected tab

--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="tabs">
+    <ul :class="classes">
         <slot></slot>
     </ul>
 </template>
@@ -16,16 +16,51 @@
             selected: {
                 type: String,
                 default: ''
+            },
+            tabsFixedWidth: {
+                type: Boolean,
+                default: false
+            },
+            swipeable: {
+                type: Boolean,
+                default: false
+            },
+            responsiveThreshold: {
+                type: Number,
+                default: Infinity
+            },
+            onShow: {
+                type: Function,
+                default: function(tabReturnValue){  }
             }
         },
 
         methods: {
             init () {
+                var options = {
+                    onShow: this.onShow,
+                    swipeable: this.swipeable,
+                    responsiveThreshold: this.responsiveThreshold               
+                };
+
                 if (!this.selected) {
-                    $(this.$el).tabs()
+                    $(this.$el).tabs(options);
                 } else {
-                    $(this.$el).tabs('select_tab', this.selected)
+                    $(this.$el).tabs(options).tabs('select_tab', this.selected);
                 }
+            }
+        },
+        computed: {
+            classes(){
+                return {
+                    'tabs': true,
+                    'tabs-fixed-width': this.tabsFixedWidth
+                };
+            }
+        },
+        watch: {
+            selected(val){
+                $(this.$el).tabs('select_tab', val);
             }
         }
     }
