ee64e1405e2b188b34c4474ee0d8f85dbf56b670
Nick Beers <nicholas.beers@gmail.com>
2017-06-22 12:52:27 -0400

    Changing timepicker slightly to conform to the picker #19

--- src/components/time-picker.vue
+++ src/components/time-picker.vue
@@ -1,5 +1,5 @@
 <template>
-    <input class="timepicker">
+    <input type="text" class="timepicker">
 </template>
 
 <script type="text/babel">
@@ -11,28 +11,12 @@
         ],
 
         props: {
-            value: {
-                type: String
-            },
             options: {
-                type: Object,
-                default: () => {
-                    return {
-                        default: 'now', // Set default time
-                        fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-                        twelvehour: true, // Use AM/PM or 24-hour format
-                        donetext: 'OK', // text for done-button
-                        cleartext: 'Clear', // text for clear-button
-                        canceltext: 'Cancel', // Text for cancel-button
-                        autoclose: false, // automatic close timepicker
-                        ampmclickable: true, // make AM PM clickable
-                        aftershow: function(){} //Function for after opening timepicker  
-                    }
-                }
+                type: Object
             }
         },
         computed: {
-            finalOptions(){
+            settings(){
                 return Object.assign({
                     default: 'now', // Set default time
                     fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
@@ -49,9 +33,7 @@
         },
         methods: {
             init () {
-                let picker = $(this.$el).pickatime(this.finalOptions); 
-
-                picker.val(this.value)
+                let picker = $(this.$el).pickatime(this.settings); 
 
                 const vm = this
                 this.$el.onchange = function () {
